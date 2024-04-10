efdc913eb91895865bf8a6db05e6238f020708f1
UNCLE-KIZARU <parsaaskari1380@gmail.com>
2024-04-10 16:39:14 +0330

    inital commit

--- Model.py
+++ Model.py
@@ -1,6 +1,25 @@
+import numpy as np
+import pandas as pd
+from tqdm.auto import tqdm
 class KNN:
     def __init__(self,X,Y):
-        self.Dataset=X
-        self.Labels=Y
-    def predict(self,X):
-        pass
+        if(type(X)!=pd.core.frame.DataFrame):
+            self.Features=pd.DataFrame(X).copy()
+        if(type(Y)!=pd.core.frame.DataFrame):
+            self.Labels=pd.DataFrame(Y,columns=["Y"]).copy()
+        self.Database=pd.concat([self.Features,self.Labels],axis=1)
+    def predict(self,X,K):
+        n,_=X.shape
+        Classes=[]
+        for i in tqdm(range(n)):
+            dist=np.linalg.norm(X[i]-self.Features.to_numpy(),axis=1)
+            
+            
+            self.Database["dist"]=dist
+            self.Database=self.Database.sort_values(by="dist")
+            
+            Class=self.Database["Y"].head(K).value_counts(ascending=False).index[0]
+            Classes.append(Class)
+
+            self.Database=self.Database.sort_index()
+        return np.array(Classes)
